569f1614a74ae00d9b2b006778492b2409274bb2
Baris Ceviz <me@barisceviz.com>
2019-04-27 01:12:37 +0300

    Add constants

---
+++ src/Minerva.Shared/Common/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Minerva.Shared.Common
+{
+    public class Constants
+    {
+        public const string BookmarkQueue = nameof(BookmarkQueue);
+        public const string EmailQueue = nameof(EmailQueue);
+    }
+}
