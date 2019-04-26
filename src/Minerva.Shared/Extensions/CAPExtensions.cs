edbbc21979c5263c23338ee8a624d56214eb95dc
Baris Ceviz <me@barisceviz.com>
2019-04-27 01:13:41 +0300

    Add Cap Extensions

---
+++ src/Minerva.Shared/Extensions/CAPExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Minerva.Shared.Data;
+
+namespace Minerva.Shared.Extensions
+{
+    public static class CapExtensions
+    {
+        public static IServiceCollection AddCap(this IServiceCollection services)
+        {
+            var configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+            services.AddCap(options =>
+            {
+                options.UseEntityFramework<MinervaDbContext>();
+                options.UsePostgreSql(configuration.GetConnectionString("DbConnection"));
+                options.UseRabbitMQ(configuration.GetConnectionString("RabbitMqConnection"));
+            });
+            return services;
+        }
+    }
+}
