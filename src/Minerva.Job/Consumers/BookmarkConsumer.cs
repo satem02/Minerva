4ef0419bd8f931e1f7a882978ec55e4b9427b72a
Baris Ceviz <me@barisceviz.com>
2019-04-27 01:11:45 +0300

    Create Consumer Project

---
+++ src/Minerva.Job/Consumers/BookmarkConsumer.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using DotNetCore.CAP;
+using Minerva.Shared.Common;
+using Minerva.Shared.Contract.Request.Bookmark;
+
+namespace Minerva.Job.Consumers
+{
+    public interface IBookmarkConsumer
+    {
+        Task ProcessBookmark(CreateBookmarkRequest request);
+    }
+
+    public class BookmarkConsumer : IBookmarkConsumer, ICapSubscribe
+    {
+        private readonly ICapPublisher _capPublisher;
+        public BookmarkConsumer(ICapPublisher capPublisher)
+        {
+            _capPublisher = capPublisher;
+        }
+
+        [CapSubscribe(Constants.BookmarkQueue)]
+        public async Task ProcessBookmark(CreateBookmarkRequest request)
+        {
+
+        }
+    }
+}
---
+++ src/Minerva.Job/Consumers/EmailConsumer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNetCore.CAP;
+using Minerva.Shared.Common;
+using Minerva.Shared.Contract.Response.Post;
+using Minerva.Shared.Services;
+
+namespace Minerva.Job.Consumers
+{
+    public interface IEmailConsumer
+    {
+        Task SendEmail(SendEmailRequest request);
+    }
+
+    public class EmailConsumer : IEmailConsumer, ICapSubscribe
+    {
+        private readonly IEmailProvider _emailProvider;
+        private readonly IPostService _postService;
+        public EmailConsumer(IEmailProvider emailProvider, IPostService postService)
+        {
+            _emailProvider = emailProvider;
+            _postService = postService;
+        }
+
+        [CapSubscribe(Constants.EmailQueue)]
+        public async Task SendEmail(SendEmailRequest request)
+        {
+            var response = await _postService.GetPostAsync(new GetPostRequest()
+            {
+                Id = request.PostId
+            });
+
+            if (!response.IsSuccess)
+            {
+                throw new Exception("Post is not found");
+            }
+
+            await _emailProvider.SendAsync(request.EmailAddress, "Kindle E-Book");
+        }
+    }
+
+    public interface IEmailProvider
+    {
+        Task SendAsync(string emailAddress, string subject);
+    }
+
+    public class SendEmailRequest
+    {
+        public int PostId { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
