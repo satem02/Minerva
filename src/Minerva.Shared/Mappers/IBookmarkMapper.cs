5a731fd68884d56ad6dd57b42c3c82c25e120461
Baris Ceviz <me@barisceviz.com>
2019-04-28 22:31:38 +0300

    Implement Mapper

--- src/Minerva.Shared/Mappers/IBookmarkMapper.cs
+++ src/Minerva.Shared/Mappers/IBookmarkMapper.cs
@@ -1,9 +1,41 @@
 ﻿using Minerva.Shared.Contract.Models;
+using Minerva.Shared.Contract.Request.Bookmark;
 using Minerva.Shared.Data.Entities;
 
 namespace Minerva.Shared.Mappers
 {
     public interface IBookmarkMapper : IMapper<BookmarkEntity, BookmarkModel>
     {
+        BookmarkEntity ToEntity(AddBookmarkRequest request);
+    }
+
+    public class BookmarkMapper : IBookmarkMapper
+    {
+        public BookmarkEntity ToEntity(BookmarkModel model)
+        {
+            return new BookmarkEntity()
+            {
+                PostId = model.PostId,
+                UserId = model.UserId
+            };
+        }
+
+        public BookmarkModel ToModel(BookmarkEntity entity)
+        {
+            return new BookmarkModel()
+            {
+                UserId = entity.UserId,
+                PostId = entity.PostId
+            };
+        }
+
+        public BookmarkEntity ToEntity(AddBookmarkRequest request)
+        {
+            return new BookmarkEntity()
+            {
+                UserId = request.UserId,
+                PostId = request.PostId
+            };
+        }
     }
 }
--- src/Minerva.Shared/Mappers/IUserMapper.cs
+++ src/Minerva.Shared/Mappers/IUserMapper.cs
@@ -8,4 +8,44 @@ namespace Minerva.Shared.Mappers
     {
         UserEntity ToEntity(RegisterRequest request);
     }
+
+    public class UserMapper : IUserMapper
+    {
+        public UserEntity ToEntity(UserModel model)
+        {
+            return new UserEntity()
+            {
+                Email = model.EmailAddress,
+                UserName = model.Username,
+                FirstName = model.FirstName,
+                MiddleName = model.MiddleName,
+                LastName = model.LastName
+            };
+        }
+
+        public UserModel ToModel(UserEntity entity)
+        {
+            return new UserModel()
+            {
+                EmailAddress = entity.Email,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                MiddleName = entity.MiddleName,
+                Username = entity.UserName
+            };
+        }
+
+        public UserEntity ToEntity(RegisterRequest request)
+        {
+            return new UserEntity()
+            {
+                MiddleName = request.MiddleName,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                Email = request.EmailAddress,
+                PhoneNumber = request.PhoneNumber
+            };
+        }
+    }
 }
